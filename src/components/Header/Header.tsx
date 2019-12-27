d2c34d82432c0d89b7b9be92cc36e7a92f2bdde6
Akshat Bais <Akshat@SGPL.LOCAL>
2019-12-27 12:37:29 +0530

    changes for drawer and mobile compatibility

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,12 @@ import './Header.css';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-
+import { IconButton, Typography, Drawer, ListItem, List, Divider } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
+  const drawerWidth = 240;
+  const [open, setOpen] = React.useState(false);
   const useStyles = makeStyles((theme: Theme) =>
     createStyles({
       root: {
@@ -17,25 +20,48 @@ function Header() {
       },
       sectionDesktop: {
         display: 'none',
-        justifyContent : 'center',
-        [theme.breakpoints.up('md')]: {
+        justifyContent: 'center',
+        [theme.breakpoints.up('sm')]: {
           display: 'flex',
         }
       },
       sectionMobile: {
-        display: 'flex',
-        justifyContent: 'center',
-        [theme.breakpoints.up('md')]: {
+        color: 'black',
+        [theme.breakpoints.down('sm')]: {
           display: 'flex',
         },
+        [theme.breakpoints.up('sm')]: {
+          display: 'none',
+        }
+      },
+      drawer: {
+        [theme.breakpoints.up('sm')]: {
+          width: drawerWidth,
+          flexShrink: 0,
+        }
+      },
+      drawerPaper: {
+        width: drawerWidth
+      },
+      dwarerItems: {
+        marginTop: '50px'
       }
     })
   );
   const classes = useStyles();
 
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  }
+
 
 
   return (
+    // <div>
     <AppBar position="sticky" classes={{ root: classes.root }}>
       <div className={classes.sectionDesktop} >
         <Toolbar classes={{ root: classes.toolBarProps }}>
@@ -49,11 +75,35 @@ function Header() {
       </div>
       <div className={classes.sectionMobile}>
         <Toolbar>
-          sdfskdfsdfdsfdsfadddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+          <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>
+          {/* <Typography>
+              Please help yourself through drawer to navigate
+          </Typography> */}
         </Toolbar>
       </div>
+      <Drawer variant="temporary" anchor='left' open={open} className={classes.drawer} classes={{
+        paper: classes.drawerPaper,
+      }} ModalProps={{ onBackdropClick: handleDrawerClose }}>
+        <div className="drawer-items">
+          <div>
+            Hello ! Again !
+          </div>
+          <div className={classes.dwarerItems}>
+            <Divider />
+            <List><a href="#" onClick={handleDrawerClose}>Home</a></List>
+            <Divider />
+            <List><a href="#experience" onClick={handleDrawerClose}> Work Experience</a></List>
+          </div>
 
-    </AppBar>
+        </div>
+      </Drawer>
+
+    </AppBar >
+
+
+
   );
 }
 export default Header;
